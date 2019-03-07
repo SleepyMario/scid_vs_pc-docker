1b59340428c0e0f9f58f51f599c846cce052b9b3
Sleepy Mario <theonesleepymario@gmail.com>
2019-03-07 14:19:14 +0100

    fix typo

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 PV=4.19
 docker run --rm -it -e DISPLAY=$DISPLAY \
-	--name scidvspc-${PV} \
+	--name scid_vs_pc-stable \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
 	-v /home/ashwin:/home/scidvspc \
 	-v /usr/local/share/scid_vs_pc:/usr/local/share/scid_vs_pc \
@@ -8,4 +8,4 @@ docker run --rm -it -e DISPLAY=$DISPLAY \
 	-v /etc/machine-id:/etc/machine-id \
 	-v /run/user/$UID/pulse/native:/tmp/pulse \
 	-v /usr/bin/stockfish:/home/ashwin/.scidvspc \
-	sleepiestmario/scid_vs_pc:4.19
+	sleepiestmario/scid_vs_pc:stable
